6516e6112fc2636d04528b6129d7b754684479d4
SaiVivekaKancharla <kancharlasai12345@gmail.com>
2024-11-17 15:21:01 -0600

    Initial commit

---
+++ Lab5MobileApp/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>My First React Native Application</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
